0ed6e130c41c014aa02063f90920b57cad046875
rohit.kumar <rohit.kumar@workday.com>
2015-01-20 19:10:35 -0800

    remove debug

--- lib/lunchfund.ex
+++ lib/lunchfund.ex
@@ -18,7 +18,6 @@ defmodule Lunchfund do
     end
     headers = %{"Content-type" => "application/x-www-form-urlencoded"}
     body = URI.encode_query([access_token: token, user_id: user, note: note, amount: -amount])
-    IO.inspect body
     {:ok, resp_body} = Venmo.post("payments", body, headers).body
     "charge" = resp_body.data.payment.action
   end
